d68eccb338d2f164736d120aeab62b02b83e2201
Alice Dias <alice.dias.pe@gmail.com>
2024-07-18 05:34:14 -0300

    first commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2014/04/02/14/10/female-306407_1280.png',
+  ),
+  '2': User(
+    id: '2',
+    name: 'Rafael',
+    email: 'rafael@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_1280.png',
+  ),
+  '3': User(
+    id: '3',
+    name: 'Fernanda',
+    email: 'fernanda@zymail.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2014/03/25/16/54/user-297566_1280.png',
+  ),
+  '4': User(
+    id: '4',
+    name: 'Guilherme',
+    email: 'guigui@abcmail.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_1280.png',
+  ),
+};
